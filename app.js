47691acc96f9c20b83c605f8db40cdb4f35745a3
Jorge Farias <jjrfarias2@gmail.com>
2023-08-16 13:32:55 -0300

    começando a testar

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('*', (req, res) => {
     res.redirect('/');
 })
 
+app.get('template-nodejs-cto9-dev.fl0.io/:username', (req,res) =>{
+    const username = req.params.username
+    res.send(console.log(`${username} - Usuário`))
+})
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
