8b214b385a61f02b8d2a1b93249b394179f46302
Jorge Farias <jjrfarias2@gmail.com>
2023-08-16 14:54:02 -0300

    validação de usuario

--- app.js
+++ app.js
@@ -2,21 +2,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
-app.use(express.urlencoded({
-    extended: true,
-}),
-)
-app.use(express.json())
+const path = require("path")
 
-app.get('/login', (req, res) => {
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+    )
+app.use(express.json())  
 
-    res.sendFile('public/index.html');
+const basePath = path.join(__dirname,"public")
+
+
+
+app.post('/public/principal' ,(req,res)=>{
+    const usuario = req.body.username
+    if (usuario=="Jorge"){
+        res.sendFile(`${basePath}/principal.html`)
+    }else{
+        res.send("Usuário incorreto!")
+    }
 })
 
-app.post('/views/index', (req,res) =>{
-    console.log(req.body)
+app.get('/' ,(req,res)=>{
+    res.sendFile(`${basePath}/index.html`)
 })
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
